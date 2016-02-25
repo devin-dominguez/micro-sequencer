95e736c3d7de4767d271a6472609eb4fa53f96c2
Devin Dominguez <dominguez.audio@gmail.com>
2016-02-25 14:19:09 -0800

    point of interest

--- frontend/components/editor/cell.jsx
+++ frontend/components/editor/cell.jsx
@@ -1,11 +1,13 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var EditorStore = require('../../stores/editorStore');
 var EditorActions = require('../../actions/editorActions');
 
 var Cell = React.createClass({
   getInitialState: function() {
     return {
-      note: null
+      note: null,
+      dragged: false
     };
   },
 
@@ -22,7 +24,7 @@ var Cell = React.createClass({
   },
 
   shouldComponentUpdate: function(nextProps, nextState) {
-    return this.state.note !== nextState.note;
+    return (this.state.note !== nextState.note) || (this.state.dragged !== nextState.dragged);
   },
 
   findSelf: function() {
@@ -56,25 +58,107 @@ var Cell = React.createClass({
     EditorActions.removeNote(this.state.note);
   },
 
-  noteContent: function() {
+  moveNoteOnDragStart: function(e) {
+    var node = ReactDOM.findDOMNode(this).firstChild;
+    //node.style.pointerEvents = "none";
 
+
+
+    var data = {
+      note: this.state.note,
+      action: "move"
+    };
+
+    e.dataTransfer.setData("noteData", JSON.stringify(data));
+    e.dataTransfer.dropEffect = "move";
+
+    this.setState({
+      dragged: true
+    });
+
+  },
+
+  resizeNoteOnDragStart: function(e) {
+    e.stopPropagation();
+    var data = {
+      note: this.state.note,
+      action: "resize"
+    };
+
+    e.dataTransfer.setData("noteData", JSON.stringify(data));
+  },
+
+  changeNoteOnDragOver: function(e) {
+    e.preventDefault();
+    console.log("!");
+  },
+
+  changeNoteOnDrop: function(e) {
+    e.preventDefault();
+
+    var data = JSON.parse(e.dataTransfer.getData("noteData"));
+    switch (data.action) {
+      case "move":
+        EditorActions.moveNoteTo(data.note, this.props.pitch, this.props.position);
+        break;
+      case "resize":
+        var newDuration = this.props.position - data.note.position + 1;
+        EditorActions.resizeNoteTo(data.note, newDuration);
+    }
+  },
+
+  preventDefault: function(e) {
+    e.preventDefault();
+  },
+
+  noteContent: function() {
     if (this.state.note !== null) {
       var width = this.state.note.duration * 100 + "%";
+
       return (
         <div className="note"
         style={{width: width}}
-        onDoubleClick={this.removeNoteOnDoubleClick}>
-          <div className="note-body" />
-          <div className="note-tail" />
+        onDoubleClick={this.removeNoteOnDoubleClick}
+        draggable="true"
+        onDragStart={this.moveNoteOnDragStart}
+        onDragOver={this.preventDefault}
+        onDrop={this.preventDefault}
+        >
+
+          <div className="note-body"
+          />
+
+          <div className="note-tail"
+          draggable="true"
+          onDragStart={this.resizeNoteOnDragStart}
+          onDragOver={this.preventDefault}
+          onDrop={this.preventDefault}
+          />
+
         </div>
       );
     }
-    return <div className="null-note" onClick={this.addNoteOnClick}/>;
+
+    return (
+        <div className="null-note"
+        onClick={this.addNoteOnClick}
+        onDragOver={this.changeNoteOnDragOver}
+        onDrop={this.changeNoteOnDrop}
+        />
+      );
   },
 
   render: function() {
+    console.log("RERENDER");
+    //if (this.state.dragged) {
+      //var style = {
+        //zIndex: "-1"
+      //};
+    //}
+
     return (
-      <li className="matrix-cell">
+      <li className="matrix-cell"
+      >
         {this.noteContent()}
       </li>
     );
